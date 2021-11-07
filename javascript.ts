ca7284dc8ce9d24497dbf1040c7bfe6dd50ff3df
Stephen Lu <stephen@heroai.ca>
2021-11-07 11:46:59 -0500

    Create javascript.ts

---
+++ javascript.ts
@@ -0,0 +1,39 @@
+/*
+
+  Implement a function that takes an ARBITRARY nested JS Object and do the following transformations:
+
+  add +1 to each Number within the Object (eg: x: 9 => x: 10)
+  add ‘AI’ to each String within the Object (eg: y: ‘hero’ => y: ‘hero AI’)
+  The object should keep its structure!
+  Note that the object can be arbitrarily nested!
+  Log the result to the browser console
+
+  // initial object
+  {
+    a: 123,
+    b: 'hero'
+    c: [1, 2, 3]
+    d: {
+      e: 3
+      f: ['abc', 'def']
+    }
+  }
+
+  // resulting object
+  {
+    a: 124,
+    b: 'hero AI'
+    c: [2, 3, 4]
+    d: {
+      e: 4
+      f: ['abc AI', 'def AI']
+    }
+  }
+
+  */
+
+
+export const transformValue = (val: any): any => {
+  // Write your solution here. You can create any number of helper functions
+  
+};
